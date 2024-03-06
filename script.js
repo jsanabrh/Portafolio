6346af6970a548548a2481ca1f12460282ecca62
jsanabrh <juanhoyos0422@gmail.com>
2024-03-05 19:08:57 -0500

    Se termina slider y se comienzan tarjetas

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
 const header = document.querySelector("header");
-let list = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let dots = document.querySelectorAll('.slider .dots li');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
 
-let active = 0;
-let lengthItems = items.length - 1;
+
 
 window.addEventListener('scroll',()=>{
     header.classList.toggle('sticky', window.scrollY > 0)
 })
 
-next.onclick = function(){
-    if(active + 1 > lengthItems){
-        active = 0;
-    }else{
-        active = active + 1
-    }
-    reloadSlider();
-}
 
-function reloadSlider(){
-    let checkLeft = items[active].offsetLeft;
-    list.computedStyleMap.left = -checkLeft + 'px';
-}
